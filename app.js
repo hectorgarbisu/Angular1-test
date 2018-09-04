a4f486f8888066e14d77ae23f72940f4953c902c
Geco <geco.hh@gmail.com>
2018-09-04 12:29:58 +0100

    this is now a mochi clicker project

--- app.js
+++ app.js
@@ -1,43 +1,14 @@
 (function () {
-    'use strict';
-    
-    var shoppingList1 = [
-      "Milk", "Donuts", "Cookies", "Chocolate", "Peanut Butter", "Pepto Bismol", "Pepto Bismol (Chocolate flavor)", "Pepto Bismol (Cookie flavor)"
-    ];
-    
-    var shoppingList2 = [
-      {
-        name: "Milk",
-        quantity: "2"
-      },
-      {
-        name: "Donuts",
-        quantity: "200"
-      },
-      {
-        name: "Cookies",
-        quantity: "300"
-      },
-      {
-        name: "Chocolate",
-        quantity: "5"
-      }
-    ];
-    
-    angular.module('ShoppingListApp', [])
-    .controller('ShoppingListController', ShoppingListController);
-    
-    ShoppingListController.$inject = ['$scope'];
-    function ShoppingListController($scope) {
-      $scope.shoppingList1 = shoppingList1;
-      $scope.shoppingList2 = shoppingList2;
-      $scope.addToList = function(){
-          var newItem = {
-            name : $scope.newItemName,
-            quantity: $scope.newItemQuantity
-          };
-          $scope.shoppingList2.push(newItem)
-      }
-    }
-    
-    })();
+  'use strict';
+  angular.module('MochiClicker', [])
+    .controller('MochiController', function ($scope) {
+      $scope.refreshPeriod = 20;
+      $scope.currentDps = 0;
+      $scope.money = 0;
+      $scope.mochis = mochis;
+      $scope.getOneMoney = function () {
+        $scope.money = $scope.money + 1;
+      };
+    });
+
+})();
---
+++ mochis.js
@@ -0,0 +1,52 @@
+var mochis = [
+    {   "name" : "mochi básico",
+        "cost" : 20,
+        "dps" : 1,
+        "description" : ""
+    },
+    {   "name" : "Mochi no tan básico",
+        "cost" : 100,
+        "dps" : 5,
+        "description" : ""
+    },
+    {   "name" : "Mochi algo mejor",
+        "cost" : 500,
+        "dps" : 25,
+        "description" : ""
+    },
+    {   "name" : "Mochi casi aceptable",
+        "cost" : 2000,
+        "dps" : 125,
+        "description" : ""
+    },
+    {   "name" : "Mochi decente",
+        "cost" : 8000,
+        "dps" : 625,
+        "description" : ""
+    },
+    {   "name" : "Mochi bueno",
+        "cost" : 32000,
+        "dps" : 3125,
+        "description" : ""
+    },
+    {   "name" : "Mochosidad linda",
+        "cost" : 96000,
+        "dps" : 15625,
+        "description" : ""
+    },
+    {   "name" : "Mochi besucón",
+        "cost" : 300000,
+        "dps" : 78125,
+        "description" : ""
+    },
+    {   "name" : "Mochosidad bella",
+        "cost" : 900000,
+        "dps" : 390625,
+        "description" : ""
+    },
+    {   "name" : "Súper mochi",
+        "cost" : 3000000,
+        "dps" : 1953125,
+        "description" : ""
+    }
+]
